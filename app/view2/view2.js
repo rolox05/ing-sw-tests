f635c3d5ec5e68a9867611ff4a8a912d5cce69ee
rolox05 <rsobisch@vmware.com>
2017-08-21 03:03:40 -0300

    Pruebas unitarias
    Rodrigo

--- app/view2/view2.js
+++ app/view2/view2.js
@@ -10,5 +10,33 @@ angular.module('myApp.view2', ['ngRoute'])
 }])
 
 .controller('View2Ctrl', [function() {
+   function suma(operando1, operando2) {
+   		if (tieneComa(operando1) || tieneComa(operando2)) return 'Error';
+   		return operando1 + operando2;
+   }
+   function resta(operando1, operando2) {
+   		if (tieneComa(operando1) || tieneComa(operando2)) return 'Error';
+   		return operando1 - operando2;
+   }
+   function multiplicacion(operando1, operando2) {
+   		if (tieneComa(operando1) || tieneComa(operando2)) return 'Error';
+   		return operando1 * operando2;
+   }
+   function division(operando1, operando2) {
+   		if (operando2 === 0) return 'Error';
+   		if (tieneComa(operando1) || tieneComa(operando2)) return 'Error';
+   		return operando1 / operando2;
+   }
 
+   // funciones privadas
+   function tieneComa(operando) {
+   	return String(operando).split('.').length > 1;
+   }
+
+   return {
+   	  suma: suma,
+   	  resta: resta,
+   	  multiplicacion: multiplicacion,
+   	  division: division
+   };
 }]);
--- app/view2/view2_test.js
+++ app/view2/view2_test.js
@@ -4,13 +4,76 @@ describe('myApp.view2 module', function() {
 
   beforeEach(module('myApp.view2'));
 
-  describe('view2 controller', function(){
-
-    it('should ....', inject(function($controller) {
-      //spec body
-      var view2Ctrl = $controller('View2Ctrl');
-      expect(view2Ctrl).toBeDefined();
+  describe('Pruebas Calculadora basica TP5', function(){
+    it('deberia sumar 5 + 6 = 11', inject(function($controller) {
+      //preparar
+    	var view2Ctrl = $controller('View2Ctrl');
+    	//actuar
+      var resul = view2Ctrl.suma(5,6);
+    	//afirmar
+      expect(resul).toBe(11);
     }));
 
+    it('No se pueden sumar numeros con coma 2.5 + 10 = "Error"', inject(function($controller) {
+      //preparar
+    	var view2Ctrl = $controller('View2Ctrl');
+    	//actuar
+      var resul = view2Ctrl.suma(2.5,10);
+    	//afirmar
+      expect(resul).toBe('Error');
+    }));
+
+    it('deberia restar 10 - 9 = 1', inject(function($controller) {
+      //preparar
+      var view2Ctrl = $controller('View2Ctrl');
+      //actuar
+      var resul = view2Ctrl.resta(10,9);
+      //afirmar
+      expect(resul).toBe(1);
+    }));
+
+	it('Multiplicar por cero = 0', inject(function($controller) {
+      //preparar
+      var view2Ctrl = $controller('View2Ctrl');
+      //actuar
+      var resul = view2Ctrl.multiplicacion(10,0);
+      //afirmar
+      expect(resul).toBe(0);
+    })); 
+
+	it('Multiplicar X x 1 = X', inject(function($controller) {
+      //preparar
+      var view2Ctrl = $controller('View2Ctrl');
+      //actuar
+      var resul = view2Ctrl.multiplicacion(1000,1);
+      //afirmar
+      expect(resul).toBe(1000);
+    }));       
+
+    it('no deberia dividir por cero', inject(function($controller) {
+      //preparar
+      var view2Ctrl = $controller('View2Ctrl');
+      //actuar
+      var resul = view2Ctrl.division(10,0);
+      //afirmar
+      expect(resul).toBe('Error');
+    }));
   });
+
+  it('deberia dividir 50 / 25 = 2', inject(function($controller) {
+      //preparar
+      var view2Ctrl = $controller('View2Ctrl');
+      //actuar
+      var resul = view2Ctrl.division(50,25);
+      //afirmar
+      expect(resul).toBe(2);
+    }));
+    it('no debe operar numeros con coma', inject(function($controller) {
+      //preparar
+      var view2Ctrl = $controller('View2Ctrl');
+      //actuar
+      var resul = view2Ctrl.suma(5.0,2.5);
+      //afirmar
+      expect(resul).toBe('Error');
+    }));
 });
